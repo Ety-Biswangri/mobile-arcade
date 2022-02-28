9f8200879fdeb8886ed4c77364ca36e9ec8beece
Ety-Biswangri <etybgc02@gmail.com>
2022-03-01 02:36:10 +0600

    all phones are loaded according to their names

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+// loading phones using phone names
 const loadPhones = () => {
-    console.log('ok');
+    const searchField = document.getElementById('search-text');
+    const searchText = searchField.value;
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayPhones(data.data))
+    // console.log(url);
+}
+
+// displaying phones using phone names
+const displayPhones = (phones) => {
+    // console.log(phones);
+    phones.forEach(phone => {
+        console.log(phone);
+    });
 }
