f4bf936b56623b77e50c7d9b3dbfe96f9033760d
Ety-Biswangri <etybgc02@gmail.com>
2022-03-01 11:09:50 +0600

    20 data are displayed

--- js/index.js
+++ js/index.js
@@ -13,19 +13,16 @@ const loadPhones = () => {
 
 // displaying phones using phone names
 const displayPhones = (phones) => {
+    // console.log(first20Phones);
     const phoneContainer = document.getElementById('phone-container');
-    // error handling
-    if (phones.length === 0) {
-        console.log('No phones are found');
-    }
-    else {
-        phones.forEach(phone => {
-            // console.log(phone);
-            const div = document.createElement('div');
-            div.classList.add('col-lg-4');
-            div.classList.add('col-12');
-            div.classList.add('mt-5');
-            div.innerHTML = `
+    const first20Phones = phones.slice(0, 20);
+    first20Phones.forEach(phone => {
+        // console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4');
+        div.classList.add('col-12');
+        div.classList.add('mt-5');
+        div.innerHTML = `
                 <div class="card" style="width: 18rem; margin: auto">
                     <img src="${phone.image}" class="card-img-top" alt="...">
                     <div class="card-body">
@@ -35,9 +32,8 @@ const displayPhones = (phones) => {
                     </div>
                 </div>
             `;
-            phoneContainer.appendChild(div);
-        });
-    }
+        phoneContainer.appendChild(div);
+    });
 }
 
 const loadDetails = (slug) => {
@@ -49,7 +45,7 @@ const loadDetails = (slug) => {
 }
 
 const displayDetails = (detail) => {
-    console.log(detail);
+    // console.log(detail);
     const phoneDetailsContainer = document.getElementById('phone-details-container');
     phoneDetailsContainer.textContent = '';
     const div = document.createElement('div');
