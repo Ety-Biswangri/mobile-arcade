03ade51b215ea13825459ab41776ea99deb4fe3a
Ety-Biswangri <etybgc02@gmail.com>
2022-03-01 17:58:51 +0600

    bonus part done

--- js/index.js
+++ js/index.js
@@ -10,6 +10,8 @@ const loadPhones = () => {
     fetch(url)
         .then(response => response.json())
         .then(data => displayPhones(data.data))
+
+    searchField.value = '';
 }
 
 // displaying phones using phone names
@@ -36,9 +38,9 @@ const displayPhones = (phones) => {
             div.classList.add('col-12');
             div.classList.add('mt-5');
             div.innerHTML = `
-                    <div class="card" style="width: 80%; margin: auto">
-                        <img src="${phone.image}" class="card-img-top" alt="...">
-                        <div class="card-body">
+                    <div class="card mb-3" style="width: 80%; margin: auto">
+                        <img src="${phone.image}" class="card-img-top m-auto mt-3 img-fluid" style="width: 50%" alt="...">
+                        <div class="card-body text-center">
                             <h5 class="card-title">${phone.phone_name}</h5>
                             <p class="card-text">Brand: ${phone.brand}</p>
                             <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary">Explore Now</Details>
@@ -61,22 +63,23 @@ const loadDetails = (slug) => {
 
 // displaying phone details using slug
 const displayDetails = (detail) => {
-    console.log(detail);
+    // console.log(detail);
     phoneDetailsContainer.textContent = '';
     const div = document.createElement('div');
     div.classList.add('col-12');
     div.classList.add('my-5');
     div.innerHTML = `
-                <div class="card" style="width: 70%; margin: auto">
+                <div class="card" style="width: 70%; margin: auto; background-color:">
 
-                    <img src="${detail.image}" style="width: 200px; margin: auto; height: 300px;  margin-top: 20px; margin-bottom: 10px; " class="card-img-top" alt="...">
+                    <img src="${detail.image}" style="width: 200px; margin: auto; height: 300px;  margin-top: 20px; margin-bottom: 10px; " class="card-img-top img-fluid" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title">${detail.name}</h5>
-                        <p class="card-text">${detail.releaseDate ? detail.releaseDate : 'No Release Date is found'}</p>
+                        <h5 class="card-title text-center">${detail.name}</h5>
+                        <p class="card-text text-center">${detail.releaseDate ? detail.releaseDate : 'No Release Date is found'}</p>
+                        <br>
 
-                        <div>
-                         <h6> <b>Main Features:</b> </h6>
+                        <div class="m-auto">
+                        <h6> <b>Main Features:</b> </h6>
                          <p> <b>Display Size:</b> ${detail.mainFeatures?.displaySize ? detail.mainFeatures.displaySize : 'No  Display Size is found'}
                          <br>
                          <b>Chipset:</b> ${detail.mainFeatures?.chipSet ? detail.mainFeatures.chipSet : 'No Chipset is found'}
@@ -87,7 +90,9 @@ const displayDetails = (detail) => {
                          <br>
                          <b>Sensors:</b> ${detail.mainFeatures?.sensors ? detail.mainFeatures.sensors : 'No Sensors are found'}
                          </p>
+                        </div>
 
+                        <div class="m-auto">
                          <p>
                          <h6> <b>Others:</b> </h6>
                          <p> <b>WLAN:</b> ${detail.others?.WLAN ? detail.others.WLAN : 'No WLAN is found'}
@@ -103,7 +108,6 @@ const displayDetails = (detail) => {
                          <b>USB:</b> ${detail.others?.USB ? detail.others.USB : 'No USB is found'}
                          </p>
                         </div>
-
                     </div>
                 </div>
             `;
