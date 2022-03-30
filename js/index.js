562742925d306a8daa2932bb342e152927d2d097
Ety-Biswangri <etybgc02@gmail.com>
2022-03-31 01:14:40 +0600

    all done

--- js/index.js
+++ js/index.js
@@ -50,7 +50,9 @@ const displayPhones = (phones) => {
                         <div class="card-body text-center">
                             <h6 class="card-title fw-bold">${phone.phone_name}</h6>
                             <p class="card-text">Brand: ${phone.brand}</p>
-                            <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary">Explore Now</Details>
+                            <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary">
+                            <a href="#" class="button-text">Explore Now</a>
+                            </Details>
                         </div>
                     </div>
                 `;
